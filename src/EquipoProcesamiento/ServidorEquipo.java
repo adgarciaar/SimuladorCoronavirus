6f335e3d43d5e354df2d0dc14e7f937f1a509911
Adrian Garcia <adriangarcia@javeriana.edu.co>
2020-04-12 20:21:14 -0500

    cambio de ejecutor a paquete entidades

--- src/EquipoProcesamiento/ServidorEquipo.java
+++ src/EquipoProcesamiento/ServidorEquipo.java
@@ -20,7 +20,7 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.concurrent.*;
-import ModeloPropagacion.EjecutorPropagacion;
+import Entidades.EjecutorPropagacion;
 import java.util.Arrays;
 import java.util.HashMap;
 
